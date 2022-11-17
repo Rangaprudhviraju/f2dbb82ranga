846c303ca60200a1def25f99c94d76c9677391aa
Rangaprudhviraju <82215129+Rangaprudhviraju@users.noreply.github.com>
2022-11-17 14:59:56 -0600

    added

--- controllers/bag.js
+++ controllers/bag.js
@@ -74,7 +74,7 @@ exports.Bag_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
-   // Handle Costume create on POST.
+   // Handle bag create on POST.
 exports.Bag_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Bag();
@@ -89,5 +89,88 @@ exports.Bag_create_post = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
-    }
+    }    
 }
+// Handle bag delete on DELETE. 
+exports.Bag_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Bag.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+ // Handle a show one view with id specified by query 
+ exports.Bag_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+    result = await Bag.findById( req.query.id) 
+    res.render('Bagdetail',  
+    { title: 'Bag Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};   
+
+//Handle a show one view with id specified by query 
+exports.Bag_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Bag.findById( req.query.id) 
+        res.render('Bagdetail',  
+{ title: 'Bag Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for creating a Bag.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Bag_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Bagcreate', { title: 'Bag Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+
+   //Handle building the view for updating a Bag. 
+// query provides the id 
+exports.Bag_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Bag.findById(req.query.id) 
+        res.render('Bagupdate', { title: 'Bag Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.Bag_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Bag.findById(req.query.id) 
+        res.render('Bagdelete', { title: 'Bag Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/Bag.js
+++ routes/Bag.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Bag', { title: 'Search Results for Bag Class' });
+router.get('/', function(req, res, next){
+ res.render('bag', { title: 'Search Results bag' });
 });
 
+var express = require('express');
+const bag_controllers= require('../controllers/bag');
+var router = express.Router();
+
+/* GET bags */
+router.get('/', bag_controllers.Bag_view_all_Page);
 module.exports = router;
+
+/* GET detail Bag page */ 
+router.get('/detail', bag_controllers.Bag_view_one_Page);
+
+ /* GET create costume page */ 
+router.get('/create', bag_controllers.Bag_create_Page);
+
+/* GET create update page */ 
+router.get('/update', bag_controllers.Bag_update_Page); 
+
+
+// GET delete costume page */ 
+router.get('/delete', bag_controllers.Bag_delete_Page); 
